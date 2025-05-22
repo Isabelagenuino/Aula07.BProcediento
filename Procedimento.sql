0c1e5b7363e811aaa0feba2582aa778a8c8e2154
Isabelagenuino <isabelagenuinodeoliveira@gmail.com>
2025-05-21 21:15:53 -0300

    Create Procedimento.sql

---
+++ Procedimento.sql
@@ -0,0 +1,36 @@
+CREATE PROCEDURE dbo.salaryHistogram @qtdIntervalo INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DECLARE @salarioMinimo AS NUMERIC (10,2);
+	DECLARE @salarioMaximo AS NUMERIC (10,2);
+	DECLARE @intervalo AS NUMERIC (10,2);
+	
+SELECT @salarioMinimo = min(salary) FROM instructor;
+SELECT @salarioMaximo = max(salary) FROM instructor;
+SET @intervalo = (@salarioMaximo-@salarioMinimo)/@qtdIntervalo;
+
+CREATE TABLE #Histograma(
+valorMinimo NUMERIC(10,2),
+valorMaximo NUMERIC(10,2),
+total INT
+);
+
+DECLARE @i INT = 0;
+WHILE @i < @qtdIntervalo
+BEGIN
+	DECLARE @valorMin NUMERIC (10,2);
+	SET @valorMin = @salarioMinimo + (@i * @intervalo);
+	DECLARE @valorMax NUMERIC (10,2);
+	SET @valorMax = @salarioMinimo + ((@i+1)* @intervalo);
+
+INSERT INTO #Histograma (valorMinimo,valorMaximo,total) SELECT @valorMin, @valorMax, count(*) FROM instructor WHERE salary >= @valorMin AND salary < @valorMax; 
+
+SET @i = @i+1;
+END
+
+UPDATE #Histograma SET total = total + (SELECT COUNT(*) FROM instructor WHERE salary = @salarioMaximo) WHERE valorMaximo = @salarioMaximo;
+SELECT * FROM #Histograma;
+DROP TABLE #Histograma;
+END
+
